6f3daf8e89b3c55b887863abf6bec04886f6417d
unknown <Diego@DESKTOP-4DG0LMC.ALEXANDER>
2024-06-16 18:52:50 -0500

    Importación de modulos y paquetes necesarios

--- limpiarData.py
+++ limpiarData.py
@@ -1,1 +1,9 @@
-print("Código para limpiar datos")
+import csv
+import pandas as pd
+import numpy as np
+import io
+from sklearn.pipeline import Pipeline
+from sklearn import preprocessing
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import RobustScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
