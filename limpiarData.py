1daf22042f14b0a3bd92c46143215682c8a6eeab
unknown <Diego@DESKTOP-4DG0LMC.ALEXANDER>
2024-06-16 19:09:53 -0500

    Abrir Base de Datos (con ERRORES)

--- limpiarData.py
+++ limpiarData.py
@@ -7,3 +7,8 @@ from sklearn import preprocessing
 from sklearn.base import TransformerMixin
 from sklearn.preprocessing import RobustScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
+
+#Importar base de datos.
+BDgen_res = pd.read_csv('./data/B_Generación_Anual_de_residuos_municipal_Distrital_2014_2021_0.csv', encoding='utf8')
+
+print(BDgen_res)
