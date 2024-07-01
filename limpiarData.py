c2e7be37d4402279c73182bd21e7704f3d884f11
djacobo@quanticotrends.com <djacobo@quanticotrends.com>
2024-06-30 20:51:15 -0500

    Filtrado de la data - Solo el año menor y mayor

--- limpiarData.py
+++ limpiarData.py
@@ -36,8 +36,18 @@ BDgen_Depart_group = BDgen_res.groupby(['DEPARTAMENTO', 'PERIODO']).agg({ #Al ag
     'POB_URBANA': 'sum',
     'POB_RURAL': 'sum',
     'QRESIDUOS_MUN': 'sum'
-}).reset_index() #Este metodo sirve para convertir a los índices de cada registro como estaban anteriormente (numéricamente) y manejarlo de una forma más fácil.
+}).reset_index() #Este metodo sirve para comenzar los índices de 0 y en orden creciente (numéricamente) y manejarlo de una forma más fácil.
 
-BDgen_Depart_group.to_csv('./data/B_Generación_Anual_de_residuos_municipal_Distrital_2014_2021_agrupado.csv', index=False, sep=',')
+# Encontrar los valores mínimos y máximos del campo 'PERIODO' por departamento
+min_max_periods = (BDgen_Depart_group.groupby('DEPARTAMENTO')['PERIODO'].agg(['min', 'max']))
 
-print(BDgen_Depart_group)
+# Obtener los valores mínimos y máximos
+min_period = min_max_periods['min'].min()
+max_period = min_max_periods['max'].max()
+
+# Filtrar las filas con los períodos mínimo y máximo
+BDgen_Depart_group_min_max = (BDgen_Depart_group[(BDgen_Depart_group['PERIODO'] == min_period) | (BDgen_Depart_group['PERIODO'] == max_period)]).reset_index().drop(columns=['index'])
+
+BDgen_Depart_group_min_max.to_csv('./data/B_Generación_Anual_de_residuos_municipal_Distrital_2014_2021_agrupado.csv', index=False, sep=',')
+
+print(BDgen_Depart_group_min_max)
