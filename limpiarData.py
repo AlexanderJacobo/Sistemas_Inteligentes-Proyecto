1a92010e67fbf0142a941a8e2deb8ef6326acb66
djacobo@quanticotrends.com <djacobo@quanticotrends.com>
2024-06-30 21:33:25 -0500

    Eliminación de creación innecesaria de archivo

--- limpiarData.py
+++ limpiarData.py
@@ -15,11 +15,6 @@ try:
         delimiter=';', #En la base de datos el delimitadore es ";"
         encoding='ISO-8859-1' #No funciona con UTF-8
     )
-    BDgen_res.to_csv(
-        './data/B_Generación_Anual_de_residuos_municipal_Distrital_2014_2021_0_modificado.csv',
-        index=False,
-        sep=','
-    )
     BDgen_res.head()
 except UnicodeDecodeError as e:
     print(f"Error al importar base de datos: {e}")
@@ -48,8 +43,6 @@ max_period = min_max_periods['max'].max()
 # Filtrar las filas con los períodos mínimo y máximo
 BDgen_Depart_group_min_max = (BDgen_Depart_group[(BDgen_Depart_group['PERIODO'] == min_period) | (BDgen_Depart_group['PERIODO'] == max_period)]).reset_index().drop(columns=['index'])
 
-BDgen_Depart_group_min_max.to_csv('./data/B_Generación_Anual_de_residuos_municipal_Distrital_2014_2021_agrupado.csv', index=False, sep=',')
+BDgen_Depart_group_min_max.to_csv('./data/Generación_Anual_de_Residuos_Municipal_Distrital_2014_2021_Peprocesado.csv', index=False, sep=',')
 
 print(BDgen_Depart_group_min_max)
-
-print("Pre procesado data 70%")
